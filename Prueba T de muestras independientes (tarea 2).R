7659265d7ee45f2746d17a752cd3911750263a04
Andrea_laptop <andrea.lunavscn@gmail.com>
2025-09-04 23:59:15 -0600

    Tarea 2

---
+++ Prueba T de muestras independientes (tarea 2).R
@@ -0,0 +1,40 @@
+data("iris")
+boxplot(iris$Sepal.Length ~ iris$Species)
+
+
+# Para usar 2 bases de datos, (subset solo funciona para 1)
+
+dat_iris <- subset(iris, Species %in% c("versicolor",
+                                           "virginica"))
+
+View(dat_iris)  # In:en tiempo real identifica TRUE los comandos que le dimos.
+
+dat_iris$Species <- droplevels(dat_iris$Species) # Para quitar "setosa" de la gráfica de la base de datos original.
+boxplot(dat_iris$Sepal.Length ~ dat_iris$Species)
+
+boxplot(setosa$Sepal.Length) # Ejemplo para ver que solo se grafica setosa.
+
+# Ejemplo correcto
+# H0 = la media de SL de virginica es mayor que la media de versicolor
+# H1 = 
+
+t.test(dat_iris$Sepal.Length ~ dat_iris$Species,
+       alternative = "greater", var.equal =T)
+5.936-6.588
+
+tapply(dat_iris$Sepal.Length, dat_iris$Species, mean)
+tapply(dat_iris$Sepal.Length, dat_iris$Species, sd)
+
+# Ejemplo erroneo, si queremos le decimos lo contrario
+# H0 = la media de SL de virginica es mayor que la media de versicolor
+# H1 = 
+
+t.test(dat_iris$Sepal.Length ~ dat_iris$Species,
+       alternative = "less", var.equal =T)
+
+# Ejemplo erroneo de 2 colas
+# H0 = la media de SL de virginica es mayor que la media de versicolor
+# H1 = 
+
+t.test(dat_iris$Sepal.Length ~ dat_iris$Species,
+       alternative = "two.sided", var.equal =T)
